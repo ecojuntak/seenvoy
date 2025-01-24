ecba384b50ab50ebce0159002e798208b5a0f89f
Eko Simanjuntak <ecojuntak@gmail.com>
2025-01-24 19:22:38 +0100

    fix: reduce timeout to 10 seconds
    Signed-off-by: Eko Simanjuntak <ecojuntak@gmail.com>

--- src/libs/envoy/axios.js
+++ src/libs/envoy/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Set config defaults when creating the instance
 const instance = axios.create({
     baseURL: '/api',
-    timeout: 1000*60*5,
+    timeout: 1000*10,
 });
 
 // Add a request interceptor
